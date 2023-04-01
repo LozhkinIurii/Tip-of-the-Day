3b4fd03c47a9b26a2f334529b448110c6b870888
Iurii_Lozhkin <lozhkinyulo@gmail.com>
2023-04-01 16:44:40 +0300

    tips count individually for each user

--- linux-tips.sh
+++ linux-tips.sh
@@ -1,31 +1,18 @@
-#! /bin/bash
-
 show_tip(){
-tip_count_path=/opt/tip_of_the_day_bash_script/tip-count.txt
-totd_tips_path=/opt/tip_of_the_day_bash_script/tips
-tip=$(cat $tip_count_path)
-tip_num="\033[93mTip number $tip\033[0m"
-echo "==============================================================="
-echo Tip of the day:
-echo -e "$tip_num"
-echo
-cat $totd_tips_path/${tip}.txt
-if [ $tip -eq 10 ]
-then
-    echo 1 > $tip_count_path
-else
-    echo $(($tip+1)) > $tip_count_path
-fi
-echo
-echo "==============================================================="
-read -p "Press 'y' to show next tip, press any other button to quit: " answer
-while [ $answer == "y" ]
-do
-    clear
+    tip_count_path=~/.tip-count
+    totd_tips_path=/opt/tip_of_the_day_bash_script/tips
+
+    if  ! [[ -s $tip_count_path && $(cat $tip_count_path) =~ ^([1-9]|10)$ ]]
+    then
+        echo 1 > $tip_count_path
+    fi
+
     tip=$(cat $tip_count_path)
     tip_num="\033[93mTip number $tip\033[0m"
-    echo "================================================================="
+    echo "==============================================================="
+    echo Tip of the day:
     echo -e "$tip_num"
+    echo
     cat $totd_tips_path/${tip}.txt
     if [ $tip -eq 10 ]
     then
@@ -33,20 +20,37 @@ do
     else
         echo $(($tip+1)) > $tip_count_path
     fi
-
     echo
-    echo "================================================================="
+    echo "==============================================================="
     read -p "Press 'y' to show next tip, press any other button to quit: " answer
-done
+    while [ $answer == "y" ]
+    do
+        clear
+        tip=$(cat $tip_count_path)
+        tip_num="\033[93mTip number $tip\033[0m"
+        echo "================================================================="
+        echo -e "$tip_num"
+        cat $totd_tips_path/${tip}.txt
+        if [ $tip -eq 10 ]
+        then
+            echo 1 > $tip_count_path
+        else
+            echo $(($tip+1)) > $tip_count_path
+        fi
 
-if [ $tip -eq 10 ]
-then
-    echo 1 > $tip_count_path
-else
-    echo $(($tip+1)) > $tip_count_path
-fi
-echo
-echo
+        echo
+        echo "================================================================="
+        read -p "Press 'y' to show next tip, press any other button to quit: " answer
+    done
+
+    if [ $tip -eq 10 ]
+    then
+        echo 1 > $tip_count_path
+    else
+        echo $(($tip+1)) > $tip_count_path
+    fi
+    echo
+    echo
 }
 
 
